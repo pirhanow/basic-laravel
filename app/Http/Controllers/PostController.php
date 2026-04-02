06241746773533de01306b3b2015698ba8f7947a
pirhanow <gutmoodx@gmail.com>
2026-04-02 21:08:02 +0300

    Finished with route and add some MyPlaceController for CRUD function

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function indexFunction()
+    {
+        return "Here is PostController";
+    }
+    public function create()
+    {
+        $postsArr = [
+            [
+                'title' => 'title is here',
+                'content' => 'content there',
+                'image' => 'is.jpg',
+                'likes' => '100',
+                'is_published' => 1,
+            ],
+            [
+                'title' => 'Another title is here',
+                'content' => 'Another content there',
+                'image' => 'Another is.jpg',
+                'likes' => '150',
+                'is_published' => 1,
+            ],
+        ];
+        foreach($postsArr as $item){
+           // dd($item);
+            Post::create($item);
+        }
+
+        dd('successfully added to db');
+    }
+
+     public function update (){
+        $post = Post::find(1, ['content']);
+
+        $post -> update ([
+                'title' => 'Updated title',
+                'content' => 'Updated content',
+                'image' => 'updated_is.jpg',
+                'likes' => 1000,
+                'is_published' => 0,
+        ]);
+        dd("updated!");
+        }
+
+        public function delete () {
+            $post = Post::withTrashed()->find(2, ['id']);
+            $post->restore();
+          //  $post->delete();
+        dd ('deleted');
+        }
+
+        public function firstOrCreate() {
+            $post = Post::find (1, ['id']);
+
+            $anotherPost = [
+                'title' => 'Some Post',
+                'content' => 'Some Content',
+                'image' => 'some image.jpg',
+                'likes' => '100',
+                'is_published' => 1,
+            ];
+           $myPost = Post::firstOrCreate([
+                 'title' => 'Some Post'
+            ],[
+                'title' => 'Some Post',
+                'content' => 'Some Content',
+                'image' => 'some image.jpg',
+                'likes' => '100',
+                'is_published' => 1,
+            ]);
+            dd('finished');
+        }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,4 +12,14 @@ Route::get('/page', function(){
     return "This is '/page'";
 });
 
-Route::get('/index', [App\Http\Controllers\MyPlaceController::class,'index']);
+// Одиночный маршрут
+Route::get('/index', [MyPlaceController::class, 'index']);
+
+// Группа маршрутов для PostController
+Route::controller(PostController::class)->group(function () {
+    Route::get('/post', 'indexFunction');
+    Route::get('/post/create', 'create');
+    Route::get('/post/update', 'update');
+    Route::get('/post/delete', 'delete');
+    Route::get('/post/first_or_create', 'firstOrCreate');
+});
